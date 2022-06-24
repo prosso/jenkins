6315acb3f93c0f8f18b7be40164b833442347513
prosso <22590678+prosso@users.noreply.github.com>
2022-06-24 17:11:44 +0200

    Update train.py

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from joblib import dump
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.linear_model import LogisticRegression
 
+#commit 1
+
 
 # Set path to inputs
 PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
